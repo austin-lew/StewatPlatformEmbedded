6bcbe7694ef0a732c50e3924510cc1bc48e682fc
Vincent <vincentvnguyen@Hotmail.com>
2024-09-20 11:47:32 -0400

    added documentation and updated dependencies

--- include/pinout.h
+++ include/pinout.h
@@ -10,16 +10,16 @@
 #define PIN_SPI_MISO (6)
 #define PIN_SPI_SCK  (7)
 
-static const uint8_t SS   = PIN_SPI_SS;
-static const uint8_t MOSI = PIN_SPI_MOSI;
-static const uint8_t MISO = PIN_SPI_MISO;
-static const uint8_t SCK  = PIN_SPI_SCK;
+// static const uint8_t SS   = PIN_SPI_SS;
+// static const uint8_t MOSI = PIN_SPI_MOSI;
+// static const uint8_t MISO = PIN_SPI_MISO;
+// static const uint8_t SCK  = PIN_SPI_SCK;
 
 #define PIN_WIRE_SDA (17)
 #define PIN_WIRE_SCL (16)
 
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
+// static const uint8_t SDA = PIN_WIRE_SDA;
+// static const uint8_t SCL = PIN_WIRE_SCL;
 
 #define LED_BUILTIN 0
 
@@ -32,14 +32,14 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 #define PIN_A6 (25)
 #define PIN_A7 (24)
 
-static const uint8_t A0 = PIN_A0;
-static const uint8_t A1 = PIN_A1;
-static const uint8_t A2 = PIN_A2;
-static const uint8_t A3 = PIN_A3;
-static const uint8_t A4 = PIN_A4;
-static const uint8_t A5 = PIN_A5;
-static const uint8_t A6 = PIN_A6;
-static const uint8_t A7 = PIN_A7;
+// static const uint8_t A0 = PIN_A0;
+// static const uint8_t A1 = PIN_A1;
+// static const uint8_t A2 = PIN_A2;
+// static const uint8_t A3 = PIN_A3;
+// static const uint8_t A4 = PIN_A4;
+// static const uint8_t A5 = PIN_A5;
+// static const uint8_t A6 = PIN_A6;
+// static const uint8_t A7 = PIN_A7;
 
 #define NUM_DIGITAL_PINS            32
 #define NUM_ANALOG_INPUTS           8
